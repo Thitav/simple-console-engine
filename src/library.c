ecf3766d81973a7fdc503d1866254b0ea929925d
Thiago Tavares <thiptav@gmail.com>
2025-03-10 22:13:00 -0300

    Major changes

--- src/library.c
+++ src/library.c
@@ -8,12 +8,92 @@
 #define BACKGROUND_WHITE (BACKGROUND_RED | BACKGROUND_GREEN | BACKGROUND_BLUE)
 #define ASCII_FULL_BLOCK 0xDB
 
-bool console_init(Console *console, const unsigned short int width, const unsigned short int height)
+bool console_buffer_init(ConsoleBuffer *console_buffer, const unsigned short int width,
+                         const unsigned short int height, const ConsoleBufferSettings settings)
+{
+	CHAR_INFO *buffer = calloc(width * height, sizeof(CHAR_INFO));
+	if (buffer == NULL)
+	{
+		// error
+		return false;
+	}
+
+	console_buffer->width = width;
+	console_buffer->height = height;
+	console_buffer->buffer = buffer;
+	console_buffer->settings = settings;
+	return true;
+}
+
+bool console_buffer_set_cell(const ConsoleBuffer *console_buffer, const unsigned short int x,
+                             const unsigned short int y)
+{
+	CHAR_INFO *cell;
+	if (x > console_buffer->width || y > console_buffer->height)
+	{
+		switch (console_buffer->settings.clip_mode)
+		{
+			case CLIP_ERROR:
+				// error
+				return false;
+			case CLIP_WRAP:
+				cell = &console_buffer->buffer[console_buffer->width * (y % console_buffer->height) +
+				                               x % console_buffer->width];
+				break;
+			default:
+				// error
+				return false;
+		}
+	}
+	else
+	{
+		cell = &console_buffer->buffer[console_buffer->width * y + x];
+	}
+
+	if (cell->Char.AsciiChar != 0)
+	{
+		switch (console_buffer->settings.overlap_mode)
+		{
+			case OVERLAP_REPLACE:
+				*cell = (CHAR_INFO){
+					.Char = console_buffer->settings.default_cell_char,
+					.Attributes = console_buffer->settings.default_cell_color
+				};
+				break;
+			case OVERLAP_XOR:
+				cell->Char.AsciiChar = 0;
+				break;
+			default:
+				// error
+				return false;
+		}
+	}
+	else
+	{
+		*cell = (CHAR_INFO){
+			.Char = console_buffer->settings.default_cell_char,
+			.Attributes = console_buffer->settings.default_cell_color
+		};
+	}
+
+	return true;
+}
+
+void console_buffer_destroy(ConsoleBuffer *console_buffer)
+{
+	free(console_buffer->buffer);
+	console_buffer->width = 0;
+	console_buffer->height = 0;
+	console_buffer->buffer = NULL;
+}
+
+bool console_init(Console *console, const unsigned short int width, const unsigned short int height,
+                  const ConsoleBufferSettings settings)
 {
 	HANDLE console_handle = GetStdHandle(STD_OUTPUT_HANDLE);
 	if (console_handle == INVALID_HANDLE_VALUE)
 	{
-		fprintf(stderr, "[ERROR][CONSOLE_INIT] Failed to get console output handle\n");
+		// error
 		return false;
 	}
 
@@ -60,8 +140,8 @@ bool console_init(Console *console, const unsigned short int width, const unsign
 		return false;
 	}
 
-	CHAR_INFO *screen_buffer = calloc(width * height, sizeof(CHAR_INFO));
-	if (screen_buffer == NULL)
+	ConsoleBuffer screen_buffer;
+	if (!console_buffer_init(&screen_buffer, width, height, settings))
 	{
 		// error
 		return false;
@@ -70,23 +150,27 @@ bool console_init(Console *console, const unsigned short int width, const unsign
 	console->handle = console_handle;
 	console->screen_buffer = screen_buffer;
 	console->window_rect = window_rect;
-	console->width = width;
-	console->height = height;
+	// console->width = width;
+	// console->height = height;
 
 	return true;
 }
 
-void console_set_pixel(const Console *console, const unsigned short int x, const unsigned short int y)
+void console_set_cell(const Console *console, const unsigned short int x, const unsigned short int y)
 {
-	console->screen_buffer[x * console->width + y] = (CHAR_INFO){
-		.Char = ASCII_FULL_BLOCK, .Attributes = FOREGROUND_WHITE | BACKGROUND_WHITE
-	};
+	console_buffer_set_cell(&console->screen_buffer, x, y, console->settings.default_cell_char,
+	                        console->settings.default_cell_color);
 }
 
-void console_update(const Console *console)
+bool console_update(const Console *console)
 {
-	WriteConsoleOutputA(console->handle, console->screen_buffer,
-	                    (COORD){(SHORT) console->width, (SHORT) console->height},
-	                    (COORD){0, 0}, (PSMALL_RECT) &console->window_rect);
+	return WriteConsoleOutputA(console->handle, console->screen_buffer.buffer,
+	                           (COORD){(SHORT) console->screen_buffer.width, (SHORT) console->screen_buffer.height},
+	                           (COORD){0, 0}, (PSMALL_RECT) &console->window_rect);
 }
 
+void console_destroy(Console *console)
+{
+	console_buffer_destroy(&console->screen_buffer);
+	// recover console
+}
--- src/library.h
+++ src/library.h
@@ -4,19 +4,47 @@
 #include <stdbool.h>
 #include <windows.h>
 
+typedef enum
+{
+  OVERLAP_REPLACE,
+  OVERLAP_XOR
+} CONSOLE_BUFFER_OVERLAP_MODE;
+
+typedef enum
+{
+  CLIP_ERROR,
+  CLIP_WRAP
+} CONSOLE_BUFFER_CLIP_MODE;
+
+typedef struct
+{
+  unsigned short int default_cell_color;
+  char default_cell_char;
+  CONSOLE_BUFFER_OVERLAP_MODE overlap_mode;
+  CONSOLE_BUFFER_CLIP_MODE clip_mode;
+} ConsoleBufferSettings;
+
+typedef struct
+{
+  CHAR_INFO *buffer;
+  unsigned short int width;
+  unsigned short int height;
+  ConsoleBufferSettings settings;
+} ConsoleBuffer;
+
 typedef struct
 {
   HANDLE handle;
-  CHAR_INFO *screen_buffer;
+  ConsoleBuffer screen_buffer;
   SMALL_RECT window_rect;
-  unsigned short int width;
-  unsigned short int height;
+  // unsigned short int width;
+  // unsigned short int height;
 } Console;
 
 bool console_init(Console *console, unsigned short int width, unsigned short int height);
 
-void console_set_pixel(const Console *console, const unsigned short int x, const unsigned short int y);
+void console_set_pixel(const Console *console, unsigned short int x, unsigned short int y);
 
-void console_update(const Console *console);
+bool console_update(const Console *console);
 
 #endif
